609c73b324d9186fc8009617bf25fae0680258c1
Yousurya <112897365+Yousurya@users.noreply.github.com>
2023-10-10 12:26:30 +0530

    code refactoring

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -54,7 +54,6 @@ export default function SignIn() {
                 </div>
 
                 {/*
-                        </button>
                     </NavLink>
                 </div> */}
             </form>
